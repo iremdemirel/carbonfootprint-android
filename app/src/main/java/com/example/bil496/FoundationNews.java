26af329dbf01488e794ec46c96d0af32537a49b2
nehiridil <46990153+nehiridil@users.noreply.github.com>
2021-03-01 11:13:40 +0300

    Foundation
    Foundation and Foundation classes are implemented

---
+++ app/src/main/java/com/example/bil496/FoundationNews.java
@@ -0,0 +1,26 @@
+package com.example.bil496;
+
+import java.util.Date;
+
+public class FoundationNews {
+    private String title;
+    private String content;
+    private Date scrapeDate;
+    //add publishDate in the future
+
+    public FoundationNews(String title, String content, Date scrapeDate){
+        this.title = title;
+        this.content = content;
+        this.scrapeDate = scrapeDate;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    //default constructor
+    public FoundationNews(){
+
+    }
+
+}
--- app/src/main/java/com/example/bil496/WebScrapingTema.java
+++ app/src/main/java/com/example/bil496/WebScrapingTema.java
@@ -10,34 +10,41 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebScrapingTema {
+    static String baseUrl = "https://www.tema.org.tr";
+    static Foundation foundation = new Foundation("TEMA",baseUrl);
     public static void scrape () {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try{
-                    String baseUrl = "https://www.tema.org.tr";
                     Document doc = Jsoup.connect(baseUrl+"/basin-odasi/basin-bultenleri").get();
                     String title = doc.title();
 
-                    //   System.out.printf("Title: " + title);
-
-                    ArrayList<String> newsList = new ArrayList<>();
                     for (Element row : doc.select("div.news a")){
                         String newsUrl = row.attributes().get("href");
                         Document news = Jsoup.connect(baseUrl + newsUrl).get();
                         String newsTitle = news.select("h1.title.font").text();
                         Elements text = news.select("div.font");
+                        String content = "";
                         for (Element e : text.get(0).getAllElements()){
                             if(e.text().length()>20){ //if text length is less then 20, skip it.
                                                         // It is probably title or just one sentence.
-                                System.out.println(e.text());
+                                content += e.text() +" ";
                             }
                         }
+                        FoundationNews newsObj = new FoundationNews(newsTitle, content, new Date());
+                        foundation.addNews(newsObj);
+                    }
+                    System.out.println("**Scraping is done");
+                    ArrayList<FoundationNews> finalbulletin = foundation.getBulletin();
+                    for(int i = 0; i<finalbulletin.size(); i++){
+                        System.out.println(finalbulletin.get(i).getTitle());
                     }
                 }catch(IOException e){
                     System.out.println(e.getMessage());
