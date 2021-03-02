f93d72fca7d44d250721a1029505f3796f2155bd
nehiridil <46990153+nehiridil@users.noreply.github.com>
2021-03-03 01:06:42 +0300

    Foundation Fragment
    Foundation Data is created.
    Foundation fragment is updated
    Greenpeace and tema logos are added

---
+++ app/src/main/java/com/example/bil496/foundations/FoundationData.java
@@ -0,0 +1,24 @@
+package com.example.bil496.foundations;
+
+import com.example.bil496.R;
+
+public class FoundationData {
+    public static String[] description = new String[]{
+            "TEMA Vakfı’nın varoluş nedeni yaşama yani toprağa sahip çıkmak ve onu korumaktır. " +
+                    "Çünkü toprak hepimizin yuvası, gıdamızın %95’inin doğrudan ya da dolaylı kaynağı, " +
+                    "habitatlarımızın dayanıklılık kaynağıdır. Okyanuslardan sonraki en büyük karbon " +
+                    "yutağı olarak toprak, iklim krizi ile mücadelenin de önemli bir aktörüdür. " +
+                    "Toprağımız varsa ormanımız, tarımımız, meralarımız ve hayvancılığımız var. " +
+                    "Toprağın 1 santimetresinin oluşması için 500 yıl gerekirken her yıl 642 milyon " +
+                    "ton toprağımız erozyona uğruyor. Yaşama ve dolayısıyla toprağa sahip çıkmayı amaç edinmiş " +
+                    "TEMA Vakfı’nın varoluş nedeni toprağı korumaktır.",
+            "Dünyada Greenpeace, küresel çevre sorunlarını ortaya çıkarmak, yeşil ve barış dolu bir " +
+                    "gelecek için gereken çözümleri sunmak amacıyla şiddetsiz ve yaratıcı çözümler sunan, " +
+                    "bağımsız kampanyalar yürüten bir organizasyondur."
+    };
+
+    public static int[] picturePath = new int[]{
+            R.drawable.tema_logo,
+            R.drawable.greenpeace_logo,
+    };
+}
