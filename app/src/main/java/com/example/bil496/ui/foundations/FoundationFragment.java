a00d3cfcbc36866666fefa64f59f4684f42e814e
nehiridil <46990153+nehiridil@users.noreply.github.com>
2021-03-03 01:09:11 +0300

    Foundation Fragment
    Foundation Fragment is updated

--- app/src/main/java/com/example/bil496/ui/foundations/FoundationFragment.java
+++ app/src/main/java/com/example/bil496/ui/foundations/FoundationFragment.java
@@ -4,32 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import com.example.bil496.MyAdapter;
 import com.example.bil496.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FoundationFragment extends Fragment {
 
-    private FoundationViewModel foundationViewModel;
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        foundationViewModel =
-                ViewModelProviders.of(this).get(FoundationViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_foundation, container, false);
-        final TextView textView = root.findViewById(R.id.text_foundation);
-        foundationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_foundation, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRecyclerView);
+        MyAdapter listAdapter = new MyAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return view;
     }
 }
