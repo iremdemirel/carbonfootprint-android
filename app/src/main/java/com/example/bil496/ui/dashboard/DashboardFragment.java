80b7968cc7107860fb9a7dcb116e794b1e76dd9c
agucsav <atakangucsav@gmail.com>
2021-03-03 22:52:56 +0300

    foundationNews title and content adapter

--- app/src/main/java/com/example/bil496/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/bil496/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.bil496.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,69 +16,81 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.bil496.R;
+import com.example.bil496.foundations.Foundation;
 import com.example.bil496.foundations.FoundationNews;
+import com.example.bil496.foundations.FoundationNewsListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.example.bil496.R.*;
 import static com.example.bil496.R.layout.*;
 
 public class DashboardFragment extends Fragment {
-    ArrayList<FoundationNews> news = new ArrayList<>();
+    final ArrayList<String> newsTitle = new ArrayList<>();
+    final ArrayList<String> newsContent = new ArrayList<>();
     FirebaseDatabase firebaseDatabase;
     DatabaseReference ref;
-
+    Context cont;
     private ListView listView;
     private View root;
+    private FoundationNewsListAdapter listAdapter;
+
     public void onActivityCreated(@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        listView = root.findViewById(id.listView);
 
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        newsTitle.add("atakan");
+        newsContent.add("deneme");
+
+        cont = getContext();
+        //dashboardViewModel = (ListView)getView().findViewById(R.id.listView);
+        root = inflater.inflate(fragment_dashboard, container, false);
+
+        listView = root.findViewById(id.lv_foundationNews);
         firebaseDatabase = FirebaseDatabase.getInstance();
-
-        final ArrayList<String> newsList = new ArrayList<>();
-        final ArrayAdapter adapter = new ArrayAdapter<String>(root.getContext(), R.layout.list_item, newsList);
-        listView.setAdapter(adapter);
-
         DatabaseReference dbRefQuoteRequestList = firebaseDatabase.getReference("Foundations").child("Green Peace")
                 .child("bulletin");
+
+        listAdapter = new FoundationNewsListAdapter(inflater);
         dbRefQuoteRequestList.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
             @Override
             public void onDataChange(final com.google.firebase.database.DataSnapshot dataSnapshot) {
-                news.clear(); // ArrayList<Pojo/Object> \\
+                newsTitle.clear(); // ArrayList<Pojo/Object> \\
+                newsContent.clear();
 
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
 
                     String content = postSnapshot.child("content").getValue(String.class);
                     String title = postSnapshot.child("title").getValue(String.class);
+                    System.out.println("title:" + title);
                     //Use the dataType you are using and also use the reference of those childs inside arrays\\
 
                     // Putting Data into Getter Setter \\
-                    /*FoundationNews fnew = new FoundationNews();
-                    fnew.setTitle(title);
-                    fnew.setContent(content);
-                    */
-                    newsList.add(title);
+                    newsTitle.add(title);
+                    newsContent.add(content);
 
                 }
-                adapter.notifyDataSetChanged();
+                listAdapter.notifyDataSetChanged();
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
         });
-    }
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        //dashboardViewModel = (ListView)getView().findViewById(R.id.listView);
-        root = inflater.inflate(fragment_dashboard, container, false);
+        String[] titlearr = Arrays.copyOf(newsTitle.toArray(), newsTitle.size(), String[].class);
+        String[] contentarr = Arrays.copyOf(newsContent.toArray(), newsContent.size(), String[].class);
+        listAdapter.setTitles(titlearr);
+        listAdapter.setContents(contentarr);
+        listView.setAdapter(listAdapter);
 
 
         /*dashboardViewModel =
