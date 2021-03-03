24b33598086ce34e082f064af7ed84c9b9dd3f98
nehiridil <46990153+nehiridil@users.noreply.github.com>
2021-03-03 13:20:10 +0300

    Greenpeace description update
    Greenpeace description is updated

--- app/src/main/java/com/example/bil496/foundations/FoundationData.java
+++ app/src/main/java/com/example/bil496/foundations/FoundationData.java
@@ -5,9 +5,7 @@ import com.example.bil496.R;
 public class FoundationData {
     public static String[] description = new String[]{
             "TEMA Vakfı’nın varoluş nedeni yaşama yani toprağa sahip çıkmak ve onu korumaktır. ",
-            "Dünyada Greenpeace, küresel çevre sorunlarını ortaya çıkarmak, yeşil ve barış dolu bir " +
-                    "gelecek için gereken çözümleri sunmak amacıyla şiddetsiz ve yaratıcı çözümler sunan, " +
-                    "bağımsız kampanyalar yürüten bir organizasyondur."
+            "Greenpeace, yeşil ve barış dolu bir gelecek için bağımsız kampanyalar yürüten bir organizasyondur."
     };
 
     public static int[] picturePath = new int[]{
