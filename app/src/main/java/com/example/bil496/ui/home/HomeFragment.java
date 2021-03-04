219da575381e391ec996140105869f9235aa8096
ahsennur <aturksoy@etu.edu.tr>
2021-03-05 00:46:07 +0300

    User info is shown on profile page.

--- app/src/main/java/com/example/bil496/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bil496/ui/home/HomeFragment.java
@@ -1,15 +1,22 @@
 package com.example.bil496.ui.home;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.bil496.LoginActivity;
 import com.example.bil496.R;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,22 +24,39 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ImageView imageView;
+    TextView name, email, bio;
+    FirebaseAuth mAuth;
+    EditText editbio;
+    AlertDialog dialog;
+    Button editBioButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        /*final TextView textView = root.findViewById(R.id.name);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+
+        mAuth = FirebaseAuth.getInstance();
+
+        imageView = (ImageView) root.findViewById(R.id.photo);
+        name = (TextView) root.findViewById(R.id.name);
+        email = (TextView) root.findViewById(R.id.email);
+        bio = (TextView) root.findViewById(R.id.bio);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Glide.with(this.getActivity())
+                .load(user.getPhotoUrl())
+                .into(imageView);
+
+        name.setText(user.getDisplayName());
+        email.setText(user.getEmail());
+        bio.setText("BIO");
 
         return root;
     }
@@ -52,5 +76,24 @@ public class HomeFragment extends Fragment {
                 Toast.makeText(getContext(), "Logout failed", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void editBio(final View view) {
+        bio = (TextView) view.findViewById(R.id.bio);
+        dialog = new AlertDialog.Builder(getActivity()).create();
+        editbio = new EditText(getActivity());
+
+        dialog.setTitle("Bio düzenle");
+        dialog.setView(editbio);
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "KAYDET", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                bio.setText(editbio.getText());
+            }
+        });
+
+        editbio.setText(bio.getText());
+        dialog.show();
     }
 }
