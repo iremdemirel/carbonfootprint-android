e86dafc4fe8e98c49ea513e60cbdc93533c52db9
iremdemirel <zdemirel@etu.edu.tr>
2021-03-06 21:41:43 +0300

    Add public transport menu spinner is completed

--- app/src/main/java/com/example/bil496/ui/carboncalculation/add_menus/add_publictransport_menu.java
+++ app/src/main/java/com/example/bil496/ui/carboncalculation/add_menus/add_publictransport_menu.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +17,7 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.bil496.R;
 
-public class add_publictransport_menu extends DialogFragment {
+public class add_publictransport_menu extends DialogFragment implements AdapterView.OnItemSelectedListener {
     private Spinner publictransport_type_spinner;
     private EditText publictransport_journey_distance;
     TextView mActionOk, mActionCancel;
@@ -23,10 +26,15 @@ public class add_publictransport_menu extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_add_publictransport_menu,null);
+        View v = inflater.inflate(R.layout.fragment_add_publictransport_menu, null);
 
         mActionOk = v.findViewById(R.id.action_ok);
         mActionCancel = v.findViewById(R.id.action_cancel);
+        publictransport_type_spinner = v.findViewById(R.id.publictransport_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.publictransport_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        publictransport_type_spinner.setAdapter(adapter);
+        publictransport_type_spinner.setOnItemSelectedListener(this);
         publictransport_journey_distance = v.findViewById(R.id.edit_publictransport_distance);
         mActionCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +51,17 @@ public class add_publictransport_menu extends DialogFragment {
             }
         });
         return v;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
 }
