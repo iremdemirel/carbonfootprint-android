3222600977a428a9938352a76dc7cf9515739f1d
iremdemirel <zdemirel@etu.edu.tr>
2021-03-07 00:02:35 +0300

    Add flight journey API integration

--- app/src/main/java/com/example/bil496/ui/carboncalculation/add_menus/add_flight_menu.java
+++ app/src/main/java/com/example/bil496/ui/carboncalculation/add_menus/add_flight_menu.java
@@ -17,10 +17,14 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.bil496.R;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class add_flight_menu extends DialogFragment implements AdapterView.OnItemSelectedListener {
-    private Spinner flight_type_spinner;
     private EditText flight_journey_distance;
     TextView mActionOk, mActionCancel;
+    String spinner_flight_text;
 
 
     @Nullable
@@ -30,7 +34,7 @@ public class add_flight_menu extends DialogFragment implements AdapterView.OnIte
 
         mActionOk = v.findViewById(R.id.action_ok);
         mActionCancel = v.findViewById(R.id.action_cancel);
-        flight_type_spinner = v.findViewById(R.id.flight_type_spinner);
+        Spinner flight_type_spinner = v.findViewById(R.id.flight_type_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.flight_types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         flight_type_spinner.setAdapter(adapter);
@@ -47,16 +51,41 @@ public class add_flight_menu extends DialogFragment implements AdapterView.OnIte
         mActionOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String input = flight_journey_distance.getText().toString();
+                final String input = flight_journey_distance.getText().toString();
                 getDialog().dismiss();
+                final Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try {
+                            OkHttpClient client = new OkHttpClient();
+
+                            Request request = new Request.Builder()
+                                    .url("https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromFlight?distance="+ input + "&type=" + spinner_flight_text)
+                                    .get()
+                                    .addHeader("x-rapidapi-key", "aaed0b37a1msh4673114dfa083a6p14111ejsnf86b67abc762")
+                                    .addHeader("x-rapidapi-host", "carbonfootprint1.p.rapidapi.com")
+                                    .build();
+
+
+                            Response response = client.newCall(request).execute();
+                            System.out.println(response.body().string());
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage() + "EEEEEEEEEEEEEEEEEEEEEEEE");
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
             }
         });
         return v;
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        spinner_flight_text = parent.getItemAtPosition(position).toString();
     }
 
     @Override
