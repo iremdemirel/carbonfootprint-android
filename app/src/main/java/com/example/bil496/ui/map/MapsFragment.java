c84e0e3c2e4e01270ddab4e34146cca86626a817
yalgun <43279137+yalgun@users.noreply.github.com>
2021-04-11 11:48:38 +0300

    Xml parsing for recycling points done.

--- app/src/main/java/com/example/bil496/ui/map/MapsFragment.java
+++ app/src/main/java/com/example/bil496/ui/map/MapsFragment.java
@@ -2,14 +2,24 @@ package com.example.bil496.ui.map;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bil496.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,7 +27,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class MapsFragment extends Fragment {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    boolean firstEntry = true;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -32,9 +59,57 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                    if (firstEntry) {
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                        firstEntry = false;
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+
+            };
+
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+            parseXML(googleMap);
+            //LatLng sydney = new LatLng(-34, 151);
+            //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            if (grantResults.length > 0) {
+                if (requestCode == 1) {
+                    if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    }
+                }
+            }
+            MapsFragment.super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     };
 
@@ -53,6 +128,49 @@ public class MapsFragment extends Fragment {
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_maps);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void parseXML(GoogleMap googleMap) {
+        InputStream inputStream = null;
+        try {
+            inputStream = getActivity().getAssets().open("data.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = null;
+
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        Document document = null;
+        try {
+            document = documentBuilder.parse(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        Element element = document.getDocumentElement();
+        element.normalize();
+
+        NodeList nodeList = document.getElementsByTagName("Placemark");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element1 = (Element) node;
+                String name = element1.getElementsByTagName("name").item(0).getTextContent();
+                String coordinates = ((Element) element1.getElementsByTagName("Point").item(0)).getElementsByTagName("coordinates").item(0).getTextContent();
+                LatLng recycleLoc = new LatLng(Double.parseDouble(coordinates.substring(0, coordinates.indexOf(",")))
+                        , Double.parseDouble(coordinates.substring(coordinates.indexOf(",") + 1,coordinates.lastIndexOf(","))));
+                googleMap.addMarker(new MarkerOptions().position(recycleLoc).title(name));
+            }
         }
     }
 }
