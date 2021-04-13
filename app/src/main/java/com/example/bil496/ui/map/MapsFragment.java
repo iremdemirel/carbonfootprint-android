16df1b21bfef2a00a500f3ed5fb8f8cadd0715ce
yalgun <43279137+yalgun@users.noreply.github.com>
2021-04-13 22:47:55 +0300

    Direction draw done.

--- app/src/main/java/com/example/bil496/ui/map/MapsFragment.java
+++ app/src/main/java/com/example/bil496/ui/map/MapsFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,6 +18,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.bil496.R;
 import com.google.android.gms.maps.CameraUpdate;
@@ -25,7 +27,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -35,16 +40,25 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, TaskLoadedCallback {
 
     LocationManager locationManager;
     LocationListener locationListener;
     boolean firstEntry = true;
+    Marker locationMarker = null;
+    Marker selectedMarker = null;
+    private Polyline currentPolyline;
+    public GoogleMap googleMapUni = null;
+    public boolean onRoute = false;
+    public Fragment thisFragment = this;
+    public ArrayList<Marker> markerList = null;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -59,17 +73,25 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            googleMapUni = googleMap;
             locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
             locationListener = new LocationListener() {
+                public LatLng lastLoc = null;
 
                 @Override
                 public void onLocationChanged(@NonNull Location location) {
-                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                    googleMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
-                    if (firstEntry) {
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
-                        firstEntry = false;
+                    if (locationMarker != null) {
+                        locationMarker.remove();
                     }
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    locationMarker = googleMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                    if (onRoute && selectedMarker != null && lastLoc.longitude != userLocation.longitude && lastLoc.latitude != userLocation.latitude) {
+                        String urlString = getUrl(locationMarker.getPosition(), selectedMarker.getPosition(), "driving");
+                        new FetchURL(getContext(), thisFragment
+                        ).execute(urlString, "driving");
+                    }
+                    lastLoc = userLocation;
+                    
                 }
 
                 @Override
@@ -99,17 +121,8 @@ public class MapsFragment extends Fragment {
             //LatLng sydney = new LatLng(-34, 151);
             //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
 
-        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-            if (grantResults.length > 0) {
-                if (requestCode == 1) {
-                    if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                    }
-                }
-            }
-            MapsFragment.super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            googleMap.setOnMarkerClickListener(marker -> onMarkerClick(marker));
         }
     };
 
@@ -159,7 +172,7 @@ public class MapsFragment extends Fragment {
 
         Element element = document.getDocumentElement();
         element.normalize();
-
+        markerList = new ArrayList<>();
         NodeList nodeList = document.getElementsByTagName("Placemark");
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
@@ -167,10 +180,112 @@ public class MapsFragment extends Fragment {
                 Element element1 = (Element) node;
                 String name = element1.getElementsByTagName("name").item(0).getTextContent();
                 String coordinates = ((Element) element1.getElementsByTagName("Point").item(0)).getElementsByTagName("coordinates").item(0).getTextContent();
-                LatLng recycleLoc = new LatLng(Double.parseDouble(coordinates.substring(coordinates.indexOf(",") + 1,coordinates.lastIndexOf(",")))
+                LatLng recycleLoc = new LatLng(Double.parseDouble(coordinates.substring(coordinates.indexOf(",") + 1, coordinates.lastIndexOf(",")))
                         , Double.parseDouble(coordinates.substring(0, coordinates.indexOf(","))));
-                googleMap.addMarker(new MarkerOptions().position(recycleLoc).title(name));
+                markerList.add(googleMap.addMarker(new MarkerOptions().position(recycleLoc).title(name)));
             }
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(getActivity(), "My Latitude: " + marker.getPosition(), Toast.LENGTH_SHORT).show();
+        List<LatLng> path = new ArrayList();
+        String urlString = getUrl(locationMarker.getPosition(), marker.getPosition(), "driving");
+        new FetchURL(getContext(), this
+        ).execute(urlString, "driving");
+        onRoute = true;
+        selectedMarker = marker;
+        return false;
+    }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                if (firstEntry) {
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                    firstEntry = false;
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+
+        };
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+        parseXML(googleMap);
+        //LatLng sydney = new LatLng(-34, 151);
+        //googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        googleMap.setOnMarkerClickListener(this);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0) {
+            if (requestCode == 1) {
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+        MapsFragment.super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null) {
+            currentPolyline.remove();
+        }
+        currentPolyline = googleMapUni.addPolyline((PolylineOptions) values[0]);
+    }
+
+    public boolean compareLocation(LatLng l1, LatLng l2) {
+        if (l1.latitude == l2.latitude) {
+            if (l1.longitude == l2.longitude) {
+                return true;
+            }
+        }
+        return false;
     }
 }
